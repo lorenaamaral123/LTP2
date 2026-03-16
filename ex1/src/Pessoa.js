f4a25f612786bf05fdca4bc878664500c9425787
lorenaamaral123 <lorena.amaral@estudante.ifms.edu.br>
2026-03-16 10:16:21 -0400

    primeiro commit

---
+++ ex1/src/Pessoa.js
@@ -0,0 +1,9 @@
+export default class Pessoa{
+    nome; 
+    idade;
+
+    constructor(nome, idade){
+        this.nome=nome;
+        this.idade=idade;
+    }
+}
---
+++ ex1/src/index.js
@@ -0,0 +1,4 @@
+import Pessoa from "./Pessoa.js";
+const pessoa1=new Pessoa ('maria', 30);
+
+console.log(pessoa1);
